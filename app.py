90d5b79bd1432e663befd9e5575fcb7691ec5ba6
Nixon <jameskayle23@gmail.com>
2024-04-30 13:16:13 +0800

    Registration + Login Complete.

--- app.py
+++ app.py
@@ -1,11 +1,166 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import random,string, hashlib
+import validation, os
+from user import Client, Admin
+from datetime import timedelta
 
 app = Flask(__name__)
 
+hehe = "".join(random.choices(string.ascii_lowercase+string.ascii_uppercase+string.digits,k=1000))
+print(hehe)
+app.secret_key = hehe
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+# Password below must be changed to match root password specified at server installation
+# Lab computers use the root password `mysql`
+app.config['MYSQL_PASSWORD'] = 'Jameskayle23!'#Password is in group chat,, idk if people can hack or not
+app.config['MYSQL_DB'] = 'pythonlogin'
+#Session Timeout
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=10)
+app.config['MYSQL_PORT'] = 3306 #DO NOTE THAT THE MYSQL SERVER INSTANCE IN THE LAB IS RUNNING ON PORT 3360.
+# Intialize MySQL
+mysql = MySQL(app)
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect(url_for("login"))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    print(request.form)
+    form = validation.LoginForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        print("hello world")
+        username = form.username.data
+        password = form.password.data
+        #Password Hashing + Salting
+        password = hashlib.sha256((password+" meow").encode())
+        password = password.hexdigest()
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM client WHERE name = %s AND password = %s",(username,password,))
+        account = cursor.fetchone()
+        if account:
+            session["loggedin"] = True
+            #Start clock for session timeout
+            session.permanent = True
+            session['id'] = account['id']
+            session["username"] = account['name']
+            global user
+            user = Client(account['id'],account["name"],account['email'],account['card'],account['membership'],account["points"])
+
+            return redirect(url_for("home"))
+        else:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT * FROM admin WHERE name = %s AND password = %s", (username, password,))
+            account = cursor.fetchone()
+            if account:
+                session["loggedin"] = True
+                # Start clock for session timeout
+                session.permanent = True
+                session['id'] = account['id']
+                session["username"] = account['name']
+                user = Admin(account["id"],account["name"],account["department"],account["position"],account["salary"],account["manager"],account["contact"],account["rating"])
+
+                return redirect(url_for("admin"))
+            else:
+                msg = "INCORRECT USERNAME/PASSWORD"
+    if request.method == "POST":
+        msg = "Login Failed"
+
+    return render_template('index.html', msg=msg, form=form)
+@app.route('/Register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    print(request.form)
+    form = validation.RegistrationForm(request.form)
+
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == "POST" and form.validate():
+        # Create variables for easy access
+        print("loll")
+        username = form.username.data
+        password = form.password.data
+        # Password Hashing + Salting
+        password = hashlib.sha256((password + " meow").encode())
+        password = password.hexdigest()
+        email = form.email.data
+
+        #Check for repeating names in Client table
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM Client, WHERE username = %s",(username,))
+        account = cursor.fetchone()
+        if account:
+            msg="Username is Taken"
+            return render_template('register.html', msg=msg, form=form)
+        #Check for repeating names in Admin
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM Admin, WHERE username = %s",(username,))
+        account = cursor.fetchone()
+        if account:
+            msg="Username is Taken"
+            return render_template('register.html', msg=msg, form=form)
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO Client VALUES (NULL, %s, %s, %s, %s, %s, %s)', (username, password, email, "0", False, 0))
+        mysql.connection.commit()
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM Client WHERE NAME = %s",(username,))
+        account = cursor.fetchone
+        mysql.connection.commit()
+
+        session["loggedin"] = True
+        # Start clock for session timeout
+        session.permanent = True
+        session['id'] = account['id']
+        session["username"] = account['NAME']
+        global user
+        user = Client(account['id'],account['NAME'],account["email"],account['card'],account["membership"])
+        return redirect(url_for("home"))
 
 
-if __name__ == "__main__":
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Errorsssss'
+        # Show registration form with message (if any)
+    return render_template('register.html', msg=msg, form=form)
+
+@app.route('/Logout')
+def logout():
+    # Remove session data, log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    del user
+    # Redirect to login page
+    return redirect(url_for('login'))
+
+@app.route("/home")
+def home():
+    if "loggedin" in session:
+        return render_template('home.html', username=session["username"])
+    return redirect(url_for("login"))
+
+@app.route('/MyWebApp/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Client WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+if __name__== '__main__':
+    os.system("cls")
     app.run(debug=True)
--- validation.py
+++ validation.py
@@ -1,15 +1,44 @@
+import string
+import re
 from wtforms import Form, StringField, validators, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+
+class LoginForm(Form):
+    username = StringField("username",[validators.Length(min=2,max=25)],render_kw={"placeholder":"Username"})
+    password = PasswordField("password", [validators.length(min=2)],render_kw={"placeholder":"Password"})
+    submit = SubmitField("Login")
+
+
 
 class RegistrationForm(Form):
-    username = StringField("Username",[validators.Length(min=4,max=25)],render_kw={"placeholder":"Username"})
-    email = StringField("Email",[validators.Length(min=6,max=35), validators.Email(message="INVALID EMAIL ADDRESS")],render_kw={"placeholder":"Email"})
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+    username = StringField("username",[validators.input_required(message='username failed')])
+    email = StringField("email",[validators.input_required(message='password failed')])
+    password = PasswordField('password', [
+        validators.input_required(),
+        validators.EqualTo('confirm', message='Passwords must match'),
     ])
-    confirm = PasswordField('Repeat Password')
+    confirm = PasswordField('ConfirmPassword')
     submit = SubmitField("Register")
-class LoginForm(Form):
-    username = StringField("Username",[validators.Length(min=2,max=25)],render_kw={"placeholder":"Username"})
-    password = PasswordField("Password", [validators.length(min=2)],render_kw={"placeholder":"Password"})
-    submit = SubmitField("Login")
+
+    def validate_password(self, field):
+        if not ((i in field.data) for i in string.ascii_lowercase):
+            raise validators.ValidationError("Password does not include lowercase")
+        elif not ((i in field.data) for i in string.ascii_uppercase):
+            raise validators.ValidationError("Password does not include uppercase")
+        elif not ((i in field.data) for i in string.digits):
+            raise validators.ValidationError("Password does not include int")
+        elif not (8 < len(field.data) < 50):
+            raise validators.ValidationError("Password does not meet length")
+
+
+    def validate_email(self, field):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if not re.match(regex, field.data):
+            print("Email failed")
+            raise validators.ValidationError("Email is invalid")
+
+    def render(self):
+        print("hello")
+        for field, error in self.errors.items():
+            print(error)
+
