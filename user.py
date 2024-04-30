f272a535603d040cd0180213e97844f4d928c5b4
Nixon <jameskayle23@gmail.com>
2024-04-30 15:44:41 +0800

    Smol edits

--- user.py
+++ user.py
@@ -1,29 +1,36 @@
-class User():
-    def __init__(self, id, name):
-        self._id = id
+class User:
+    def __init__(self, idno, name):
+        self._id = idno
         self._name = name
 
     def get_name(self):
         return self._name
 
+    def __del__(self):
+        del self
+
+
 class Admin(User):
-    def __init__(self,id:int,name:str,department:str,position:str,salary:float,manager:bool,contact:int,rating:float):
-        super().__init__(id,name)
+    def __init__(self, idno: int, name: str, department: str, position: str,
+                 salary: float, manager: bool, contact: int, rating: float):
+        super().__init__(idno, name)
         self._department = department
         self._pos = position
         self._salary = salary
         self._manager = manager
-        self._contact= contact
+        self._contact = contact
         self.rating = rating
 
+
 class Client(User):
-    def __init__(self,id:int, name:str, email:str, card:int, membership:bool, points:int):
-        super().__init__(id,name)
+    def __init__(self, idno: int, name: str, email: str, card: int, membership: bool, points: int):
+        super().__init__(idno, name)
         self._email = email
-        try:
-            self._cardno = card[:15]
-            self._cvv = card[15:]
-        except:
+        if len(str(card)) =
+    = 18:
+            self._cardno = int(str(card)[:15])
+            self._cvv = int(str(card)[15:])
+        else:
             self._cardno = None
             self._cvv = None
         self._membership = membership
--- validation.py
+++ validation.py
@@ -1,44 +1,34 @@
-import string
+
 import re
 from wtforms import Form, StringField, validators, PasswordField, SubmitField
-from flask_wtf import FlaskForm
+
 
 class LoginForm(Form):
-    username = StringField("username",[validators.Length(min=2,max=25)],render_kw={"placeholder":"Username"})
-    password = PasswordField("password", [validators.length(min=2)],render_kw={"placeholder":"Password"})
+    username = StringField("username", [validators.Length(min=2, max=25)], render_kw={"placeholder": "Username"})
+    password = PasswordField("password", [validators.length(min=2)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Login")
 
 
-
 class RegistrationForm(Form):
-    username = StringField("username",[validators.input_required(message='username failed')])
-    email = StringField("email",[validators.input_required(message='password failed')])
-    password = PasswordField('password', [
-        validators.input_required(),
-        validators.EqualTo('confirm', message='Passwords must match'),
-    ])
-    confirm = PasswordField('ConfirmPassword')
+    username = StringField("username")
+    email = StringField("email")
+    password = PasswordField('password')
     submit = SubmitField("Register")
 
+    @staticmethod
     def validate_password(self, field):
-        if not ((i in field.data) for i in string.ascii_lowercase):
-            raise validators.ValidationError("Password does not include lowercase")
-        elif not ((i in field.data) for i in string.ascii_uppercase):
-            raise validators.ValidationError("Password does not include uppercase")
-        elif not ((i in field.data) for i in string.digits):
+        if re.search('[0-9]', field.data) is None:
             raise validators.ValidationError("Password does not include int")
+        elif re.search('[A-Z]', field.data) is None:
+            raise validators.ValidationError("Password does not include uppercase")
+        elif re.search('[a-z]', field.data) is None:
+            raise validators.ValidationError("Password does not include Lowercase")
         elif not (8 < len(field.data) < 50):
             raise validators.ValidationError("Password does not meet length")
 
-
+    @staticmethod
     def validate_email(self, field):
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         if not re.match(regex, field.data):
             print("Email failed")
             raise validators.ValidationError("Email is invalid")
-
-    def render(self):
-        print("hello")
-        for field, error in self.errors.items():
-            print(error)
-
