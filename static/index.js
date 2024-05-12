dbe2e6f3b907c9762d2a1646917b0d7f2c1bf80f
L30NonCracc <leonard.tan.junq@gmail.com>
2024-05-12 11:59:53 +0800

    v1
    updated webcam feature for front end

--- static/index.js
+++ static/index.js
@@ -1,18 +1,33 @@
-var video = document.createElement('video');
-video.setAttribute('playsinline', '');
-video.setAttribute('autoplay', '');
-video.setAttribute('muted', '');
-video.style.width = '200px';
-video.style.height = '200px';
+document.getElementById('toggle-webcam').addEventListener('click', function() {
+    var videoExists = document.querySelector('.login-background video');
+    if (videoExists) {
+        videoExists.remove();
+        document.getElementById('background-image').style.display = 'block';
+    } else {
+        var video = document.createElement('video');
+        video.setAttribute('playsinline', '');
+        video.setAttribute('autoplay', '');
+        video.setAttribute('muted', '');
+        video.style.width = '100%';
+        video.style.height = '100%';
 
-var facingMode = "user";
-var constraints = {
-  audio: false,
-  video: {
-   facingMode: facingMode
-  }
-};
+        var constraints = {
+            audio: false,
+            video: {
+                facingMode: "user"
+            }
+        };
 
-navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-  video.srcObject = stream;
+        console.log(document.getElementById('background-container'));
+        document.getElementById('background-container').appendChild(video);
+
+
+        navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+            video.srcObject = stream;
+            document.getElementById('background-image').style.display = 'none';
+            document.getElementById('background-container').appendChild(video);
+        }).catch(function(error) {
+            console.error("Error accessing media devices.", error);
+        });
+    }
 });
